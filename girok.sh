56a3bf4d9c94818e9d23cb0d4ae5aaa60f0772d1
Jaeho Shin <netj@sparcs.org>
2007-01-01 23:49:34 +0900

    bugfix: period may be 0
    darcs-hash:20070101144934-946c4-941271dbdee2388147a9ce5d819085e851ac0b08.gz

--- girok.sh
+++ girok.sh
@@ -365,6 +365,7 @@ EOF
     local periodcnt=${periodspec#*/}
     local period=`date +"$periodfmt" | sed -e 's/^0*//g'`
     period=`printf "%0${#periodcnt}d" $(($period % $periodcnt))`
+    period=${period:-0}
     # determine whether to restart or not
     local restart=false
     if [ "$period@$periodspec" != "`cat "$prefix.last" 2>/dev/null`" ]; then
